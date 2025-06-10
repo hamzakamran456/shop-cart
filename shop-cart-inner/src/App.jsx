35f2c645c938f4ce77218109037263055c2a195e
Hamza Kamran <hamzakamrn8@gmail.com>
2025-06-10 17:48:08 +0500

    create pages

---
+++ shop-cart-inner/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <nav>Navbar</nav>
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
