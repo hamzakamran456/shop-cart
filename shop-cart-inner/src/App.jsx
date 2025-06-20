6ba14f5a93df6b4f028da989d2c1ea91384ff49f
Hamza Kamran <hamzakamrn8@gmail.com>
2025-06-20 17:55:15 +0500

    home page banner,home category and selected category completed

--- shop-cart-inner/src/App.jsx
+++ shop-cart-inner/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navitems from "./components/Navitems";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <nav>Navbar</nav>
+      <Navitems />
       <Outlet />
     </>
   );
