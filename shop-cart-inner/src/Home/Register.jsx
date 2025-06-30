4032659c963111b5475fbbb30f1df2a4c2dc25fd
Hamza Kamran <hamzakamrn8@gmail.com>
2025-06-30 10:09:53 +0500

    register and locate separate page

--- shop-cart-inner/src/Home/Register.jsx
+++ shop-cart-inner/src/Home/Register.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const subTitle = "Save The Day";
 const title = (
-  <h2>
+  <h2 className="title">
     Join on Day Long Free Workshop for{" "}
     <b>
       Advance <span>Mastering</span> on Sales.
@@ -15,7 +15,43 @@ const Register = () => {
   return (
     <section className="register-section padding-tb pb-0">
       <div className="container">
-        <div></div>
+        <div className="row g-4 row-cols-lg-2 row-cols-lg align-items-center">
+          <div className="col">
+            <div className="section-header">
+              <span className="subtitle">{subTitle}</span>
+              {title}
+              <p>{desc}</p>
+            </div>
+          </div>
+          <div className="col">
+            <div className="section-wrapper">
+              <h4>Register Now</h4>
+              <form className="register-form">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Username"
+                  className="reg-input"
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className="reg-input"
+                />
+                <input
+                  type="number"
+                  name="number"
+                  placeholder="Phone"
+                  className="reg-input"
+                />
+                <button type="submit" className="lab-btn">
+                  Register Now
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
