17c50503017e264ab7fab3d0cc983f80dc644c3a
Hamza Kamran <hamzakamrn8@gmail.com>
2025-07-11 17:48:15 +0500

    shop in process

--- shop-cart-inner/src/shop/Shop.jsx
+++ shop-cart-inner/src/shop/Shop.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import PageHeader from "../components/PageHeader";
-const showResults = "Showing 01 - 12 of 139 Results"
+import Data from "../products.json";
+import ProductCards from "./ProductCards";
+const showResults = "Showing 01 - 12 of 139 Results";
+
 const Shop = () => {
+  const [GridList, setGridList] = useState(true);
+  const [products, setProducts] = useState(Data);
+  
+
   return (
     <div>
       <PageHeader title="Our Shop Page" curPage="Shop" />
@@ -11,7 +18,22 @@ const Shop = () => {
             <div className="col-lg-8 col-12">
               <article>
                 <div className="shop-title d-flex flex-wrap justify-content-between">
-                    <p>{showResults}</p>
+                  <p>{showResults}</p>
+                  <div
+                    className={`product-view-mode ${
+                      GridList ? "gridActive" : "listActive"
+                    }`}
+                  >
+                    <a className="grid" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-ghost"></i>
+                    </a>
+                    <a className="list" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-listine-dots"></i>
+                    </a>
+                  </div>
+                </div>
+                <div>
+                  <ProductCards GridList={GridList} products={products} />
                 </div>
               </article>
             </div>
